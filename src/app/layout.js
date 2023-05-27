4a9b0c1d465f3594d07ae25aac46b0009e5b4121
Mifaresss <9803211@gmail.com>
2023-05-28 01:39:35 +0300

    Test changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,9 @@
+'use client'
 import { Header } from '@/components/Header/Header'
 import { Provider } from 'react-redux'
 import './globals.css'
 import './reset.css'
 import { store } from '@/store'
-// import { Inter } from 'next/font/google'
-
-// const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-   title: 'Food delivery',
-   description: 'Food delivery app created by Serafim Barey',
-}
 
 export default function RootLayout({ children }) {
    return (
